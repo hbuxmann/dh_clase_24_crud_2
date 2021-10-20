0f6021db69c93411f67ea509fd9f0efeb8896d47
Herni <51058546+hbuxmann@users.noreply.github.com>
2021-10-20 19:31:07 -0300

    Delete app2.js

--- src/app2.js
+++
@@ -1,7 +0,0 @@
-// ************ Require's ************
-// const createError = require('http-errors');
-// const cookieParser = require('cookie-parser');
-const express = require('express');
-// const logger = require('morgan');
-// const path = require('path');
-// const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
